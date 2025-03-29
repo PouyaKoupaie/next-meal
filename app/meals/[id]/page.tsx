21a58d338a60cd5c875ff64a5d09db97b7342a11
Pouya <titanpooyanote@gmail.com>
2025-03-29 23:53:24 +0330

    add food form added

--- app/meals/[id]/page.tsx
+++ app/meals/[id]/page.tsx
@@ -1,8 +1,31 @@
-const MealDetail = ({params}) => {
+import Image from 'next/image'
+import style from './page.module.css'
+import { getMeal } from '@/lib/meals'
+import { notFound } from 'next/navigation'
+const MealDetail = ({params}: { params: { id: string } }) => {
+  const meal = getMeal(params.id)
+  if (!meal) {
+    notFound()
+  }
+  meal.instructions = meal.instructions.replace(/<[^>]+>/g, '<br />')
   return (
-    <div>
-      {params.id}
-    </div>
+    <>
+    <header className={style.header}>
+      <div className={style.image}>
+        <Image src={meal.image} fill alt=''/>
+      </div>
+      <div className={style.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={style.creator}>
+          by <a href={`mail to: ${meal.creator_email}`}>{meal.creator}</a>
+        </p>
+        <p className={style.summary}></p>
+      </div>
+    </header>
+    <main>
+      <p className={style.instructions} dangerouslySetInnerHTML={{__html:meal.instructions}}></p>
+    </main>
+    </>
   )
 }
 
--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,9 +1,64 @@
-const Share = () => {
-  return (
-    <div>
-      share
-    </div>
-  )
-}
+import ImagePicker from '@/components/meals/image-picker';
+import classes from './page.module.css';
+import { title } from 'process';
 
-export default Share
+export default function ShareMealPage() {
+  async function shareMeal(formData: FormData) {
+    'use server';
+
+    const meal = {
+      title: formData.get('title') as string,
+      summary: formData.get('summary') as string,
+      instructions: formData.get('instructions') as string,
+      image: formData.get('image') as string,
+      creator: formData.get('name') as string,
+      creator_email: formData.get('email') as string,
+    }
+    console.log(meal);
+  }
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form} action={shareMeal}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker label='you email' name='image'/>
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
+}
